f435f9e6b899a1a13989aaa7981fa95e80b9f3cd
llei074 <llei074@aucklanduni.ac.nz>
2024-05-16 17:50:00 +1200

    Added a switch case to check user input values

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,11 +6,32 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private Integer roundNumber;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    roundNumber = 1;
   }
 
-  public void play() {}
+  public void play() {
+
+
+    MessageCli.START_ROUND.printMessage(roundNumber.toString());
+    MessageCli.ASK_INPUT.printMessage();
+    String input = Utils.scanner.nextLine();
+    switch (input) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+        break;
+      default:
+        MessageCli.INVALID_INPUT.printMessage();
+        break;
+    }
+  }
 
   public void endGame() {}
 
