f19da70cef1ae15024653827fc51e0a21902dd34
llei074 <llei074@aucklanduni.ac.nz>
2024-05-17 12:43:46 +1200

    Added HardBot to use random strategy

---
+++ src/main/java/nz/ac/auckland/se281/HardBot.java
@@ -0,0 +1,24 @@
+package nz.ac.auckland.se281;
+
+public class HardBot implements Robot{
+    private Strategy strategy;
+
+    public HardBot() {
+        this.strategy = new RandomStrategy();
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    @Override
+    public String getRobotOutput() {
+        return strategy.getAction();
+    }
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+  
+}
